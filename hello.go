519f11ffe2ba599dd462aba929349f80b475ee65
NaoyaNakanishi <naoya@NaoyaNakanishinoMacBook-ea.local>
2021-09-25 02:02:34 +0900

    set up fyne

--- hello.go
+++ hello.go
@@ -1,59 +1,17 @@
 package main
 
-import "fmt"
-
-// General is all type data.
-type General interface{}
-
-// GData is holding General value.
-type GData interface {
-	Set(nm string, g General) GData
-	Print()
-}
-
-// NData is structure.
-type NData struct {
-	Name string
-	Data int
-}
-
-// Set is NData method.
-func (nd *NData) Set(nm string, g General) GData {
-	nd.Name = nm
-	nd.Data = g.(int)
-	return nd
-}
-
-// Print is NData method.
-func (nd *NData) Print() {
-	fmt.Printf("<<%s>> value: %d\n", nd.Name, nd.Data)
-}
-
-// SData is structure.
-type SData struct {
-	Name string
-	Data string
-}
-
-// Set is SData method.
-func (sd *SData) Set(nm string, g General) GData {
-	sd.Name = nm
-	sd.Data = g.(string)
-	return sd
-}
-
-// Print is SData method.
-func (sd *SData) Print() {
-	fmt.Printf("* %s [%s] *\n", sd.Name, sd.Data)
-}
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	var data = []GData{}
-	data = append(data, new(NData).Set("Taro", 123))
-	data = append(data, new(SData).Set("Jiro", "hello!"))
-	data = append(data, new(NData).Set("Hanako", 98700))
-	data = append(data, new(SData).Set("Sachiko", "happy?"))
-	for _, ob := range data {
-		ob.Print()
-	}
+	a := app.New()
+
+	w := a.NewWindow("Hello")
+	w.SetContent(
+		widget.NewLabel("Hello Fyne!"),
+	)
+
+	w.ShowAndRun()
 }
